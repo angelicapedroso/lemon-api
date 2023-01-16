9154b80c0c897704b8aca06da3ba15b1bafc30af
angelicapedroso <angelicapedroso05@gmail.com>
2023-01-15 23:22:22 -0300

    refactor: altera nome do arquivo

--- src/tests/mocks/elegibilityMocks.js
+++ src/tests/mocks/elegibilityMocks.js
@@ -112,9 +112,36 @@ const notElegibleByMinConsumption = {
   },
 };
 
+const invalidInput = {
+  input: {
+    numeroDoDocumento: '140417706',
+    tipoDeConexao: 'bifasico',
+    classeDeConsumo: 'comercial',
+    modalidadeTarifaria: 'convencional',
+    historicoDeConsumo: [
+      3878,
+      9760,
+      5976,
+      2797,
+      2481,
+      5731,
+      7538,
+      4392,
+      7859,
+      4160,
+      6941,
+      4597,
+    ],
+  },
+  output: {
+    message: 'mensagem de erro',
+  },
+};
+
 module.exports = {
   elegible,
   notElegibleByConsumptionClass,
   notElegibleByTariffModality,
   notElegibleByMinConsumption,
+  invalidInput,
 };
